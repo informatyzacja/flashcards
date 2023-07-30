7e42dd62977bfb0bd89ef71fbeb5b0d2c8e477ed
Michał Korczak <michal.k2001@gmail.com>
2023-07-30 14:40:55 +0200

    feat: init chakra (#2)

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import Layout from '@/components/Layout';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>Fiszki</title>
+        <meta name="description" content="Twoje fiszki w każdym miejscu" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;

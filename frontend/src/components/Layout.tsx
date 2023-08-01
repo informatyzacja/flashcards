792c156a8919af090c0ae0944e09e15fc007340c
Suchy702 <wojtek.suchy6@wp.pl>
2023-08-01 16:25:40 +0200

    build: (first trying to build navbar)

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,6 +2,8 @@ import { chakra, Container, VStack } from '@chakra-ui/react';
 import { Lato } from 'next/font/google';
 import type React from 'react';
 
+import Navbar from './Navbar';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -16,8 +18,11 @@ const lato = Lato({
 export default function Layout({ children }: LayoutProps) {
   return (
     <VStack minH="100vh" spacing={8} className={lato.variable}>
-      <Container maxW="container.xl" flex={1} display="flex" flexDir="column">
-        <chakra.main>{children}</chakra.main>
+      <Container maxW="container.full" flex={1} display="flex" flexDir="column">
+        <chakra.main>
+          <Navbar />
+          {children}
+        </chakra.main>
       </Container>
     </VStack>
   );
